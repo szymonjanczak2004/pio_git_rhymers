fc1367a98b5ac05c27f35390246b3cca9f9b412e
255631-2 <wiktor.cj123@gmail.com>
2026-04-22 21:53:09 +0200

    6, 7, 8. Stale, final i override

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,11 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int CAPACITY = 12;
+    public static final int EMPTY_STACK_VALUE = -1;
+    private final int[] numbers = new int[CAPACITY];
 
-    public int total = -1;
+    public int total = EMPTY_STACK_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
