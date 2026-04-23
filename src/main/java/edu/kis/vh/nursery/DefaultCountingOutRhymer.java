e9d26fff6dc1715e556c1eceb08d5a84385b7b66
szymonjanczak2004 <255609@edu.p.lodz.pl>
2026-04-23 03:37:57 +0200

    punkty 9-12

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -13,6 +13,10 @@ public class DefaultCountingOutRhymer {
             numbers[++total] = in;
     }
 
+    public int getTotal() {
+        return total;
+    }
+
     public boolean callCheck() {
         return total == EMPTY_RHYMER_INDICATOR;
     }
--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    public final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+    private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
     
     public int countOut() {
         while (!callCheck())
