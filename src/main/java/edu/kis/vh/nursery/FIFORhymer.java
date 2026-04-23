d841ed59dcd9896bafaf2144481774bd6d44c3cb
szymonjanczak2004 <255609@edu.p.lodz.pl>
2026-04-23 04:22:25 +0200

    17. dodanie dokumentacji javadoc

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,9 @@
 package edu.kis.vh.nursery;
-
+/**Klasa reprezentujaca wyliczanke w oparciu o kolejke FIFO**/
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
-    
+    /**Zwraca i usuwa najstarszy element**/
     public int countOut() {
         while (!callCheck())
             temp.countIn(super.countOut());
